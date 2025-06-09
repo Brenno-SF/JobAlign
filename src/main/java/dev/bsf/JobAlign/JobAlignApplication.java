4c320afa6e296787ef661e363ee6a8d006d25e94
Brenno-SF <brprogrammersf@gmail.com>
2025-06-09 17:56:54 -0300

    initial commit

---
+++ src/main/java/dev/bsf/JobAlign/JobAlignApplication.java
@@ -0,0 +1,13 @@
+package dev.bsf.JobAlign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAlignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAlignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bsf/JobAlign/JobAlignApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bsf.JobAlign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAlignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
